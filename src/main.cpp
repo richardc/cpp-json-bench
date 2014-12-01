0f8fbacedab014fc33344e56ebdad984a2b7f8ba
Richard Clamp <richardc@unixbeard.net>
2014-12-01 16:07:52 +0000

    run more smaller batches

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,20 @@
 #include <iterator>
 #include <string>
 
-BENCHMARK(RapidJson, ParseFile, 2, 10) {
+BENCHMARK(RapidJson, ParseFile, 20, 1) {
     FILE *fp = fopen("MOCK_DATA.json" , "r");
     rapidjson::FileStream is(fp);
     rapidjson::Document document;
     document.ParseStream<0>(is);
 }
 
-BENCHMARK(PicoJson, ParseFile, 2, 10) {
+BENCHMARK(PicoJson, ParseFile, 20, 1) {
     std::ifstream fs("MOCK_DATA.json");
     picojson::value v;
     fs >> v;
 }
 
-BENCHMARK(JsonCPP, ParseFile, 2, 10) {
+BENCHMARK(JsonCPP, ParseFile, 20, 1) {
     std::ifstream fs("MOCK_DATA.json");
     Json::Value v;
     Json::Reader r;
