5f3ede17894d807bf6944517c967929765520f8f
Richard Clamp <richardc@unixbeard.net>
2014-12-01 15:41:04 +0000

    picojson and rapidjson parsefile comparison

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <hayai.hpp>
- 
-int main() { 
+#include <rapidjson/document.h>
+#include <rapidjson/filestream.h>
+#include <picojson.h>
+#include <fstream>
+#include <iterator>
+#include <string>
+
+BENCHMARK(RapidJson, ParseFile, 2, 10) {
+    FILE *fp = fopen("MOCK_DATA.json" , "r");
+    rapidjson::FileStream is(fp);
+    rapidjson::Document document;
+    document.ParseStream<0>(is);
+}
+
+BENCHMARK(PicoJson, ParseFile, 2, 10) {
+    std::ifstream fs("MOCK_DATA.json");
+    picojson::value v;
+    fs >> v;
+}
+
+int main() {
     hayai::ConsoleOutputter consoleOutputter;
-         
+
     hayai::Benchmarker::AddOutputter(consoleOutputter);
     hayai::Benchmarker::RunAllTests();
     return 0;
